2b86fb94595dd32f7fb2d88d96f55e4ee7fbddb5
jslee <jshiun@ymail.com>
2021-07-17 13:56:31 +0800

    Implement frontend script

--- website/index.js
+++ website/index.js
@@ -1,11 +1,23 @@
-fetch('http://localhost:5000')
-    .then(function (response) {
-        if (response.status === 200) console.log('%cConnected to backend!', 'color: green');
-        return response.json();
-    })
-    .then(function (json) {
-        console.log(json);
-    })
-    .catch(function (error) {
-        console.error('Failed to connect to backend! Error: ', error);
-    });
+
+
+window.addEventListener("DOMContentLoaded", ()=> {
+    const btnSubmit = document.getElementById("btnSubmit");
+    btnSubmit.addEventListener("click", ()=> {
+        const guessVal = document.getElementById("txtGuess").value;
+        // alert(guessVal);
+        // Send HTTP request
+        fetch('http://localhost:5000/guess/' + guessVal, {'method':'POST'})
+        .then(function (response) {
+            // console.log(response)
+            if (response.status === 201) console.log('%cConnected to backend!', 'color: green');
+            return response.json(); // convert body to JSON and return this value.
+        })
+        .then(function (json) {
+            // console.log(json)
+            alert(json.message);
+        })
+        .catch(function (error) {
+            console.error('Failed to connect to backend! Error: ', error);
+        }); // End of fetch fn
+    }); // End of "click" event fn
+}); // End of "load" event fun
